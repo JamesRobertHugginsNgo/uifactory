3270bab599941e89826f4e07cc42a5d202e2ca77
James Robert Huggins Ngo <JamesRobert.Ngo@GMail.com>
2020-01-05 13:17:04 -0500

    Add Project Boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+const del = require('del');
+const gulp = require('gulp');
+
+const babel = require('gulp-babel');
+const connect = require('gulp-connect');
+const eslint = require('gulp-eslint');
+const sass = require('gulp-sass');
+
+////////////////////////////////////////////////////////////////////////////////
+
+function cleanup() {
+	return del('dist');
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+const buildJsSrc = ['src/**/*.js'];
+function buildJs() {
+	return gulp.src(buildJsSrc, { since: gulp.lastRun(buildJs) })
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(babel())
+		.pipe(gulp.dest('dist'));
+}
+function watchJs() {
+	gulp.watch(buildJsSrc, buildJs);
+}
+
+const buildScripts = gulp.parallel(buildJs);
+const watchScripts = gulp.parallel(watchJs);
+
+////////////////////////////////////////////////////////////////////////////////
+
+const buildStyleCssSrc = ['src/**/*.css'];
+function buildStyleCss() {
+	return gulp.src(buildStyleCssSrc, { since: gulp.lastRun(buildStyleCss) })
+		.pipe(gulp.dest('dist'));
+}
+function watchStyleCss() {
+	gulp.watch(buildStyleCssSrc, buildStyleCss);
+}
+
+const buildStyleScssSrc = ['src/**/*.scss'];
+function buildStyleScss() {
+	return gulp.src(buildStyleScssSrc, { since: gulp.lastRun(buildStyleScss) })
+		.pipe(sass())
+		.pipe(gulp.dest('dist'));
+}
+function watchStyleScss() {
+	gulp.watch(buildStyleScssSrc, buildStyleScss);
+}
+
+const buildStyles = gulp.parallel(buildStyleCss, buildStyleScss);
+const watchStyles = gulp.parallel(watchStyleCss, watchStyleScss);
+
+////////////////////////////////////////////////////////////////////////////////
+
+const buildHtmlSrc = ['src/**/*.html'];
+function buildHtml() {
+	return gulp.src(buildHtmlSrc, { since: gulp.lastRun(buildHtml) })
+		.pipe(gulp.dest('dist'));
+}
+function watchHtml() {
+	gulp.watch(buildHtmlSrc, buildHtml);
+}
+
+const buildDocuments = gulp.parallel(buildHtml);
+const watchDocuments = gulp.parallel(watchHtml);
+
+////////////////////////////////////////////////////////////////////////////////
+
+exports.default = gulp.series(cleanup, gulp.parallel(buildScripts, buildStyles, buildDocuments));
+
+const watch = gulp.parallel(watchScripts, watchStyles, watchDocuments);
+exports.watch = gulp.series(exports.default, watch);
+
+function serve() {
+	connect.server({
+		livereload: true,
+		port: 8080,
+		root: '.'
+	});
+}
+exports.serve = gulp.series(exports.default, gulp.parallel(serve, watch));
